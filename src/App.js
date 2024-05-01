9d580f7eb2da3d81c013951e559c05029d23f233
Aarush <aarush@gmail.com>
2024-05-01 09:57:13 -0500

    Markdown Editor first version

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
-const urlParams = new URLSearchParams(window.location.search);
-const limit = urlParams.get("limit") || 6;
+
 const App = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch(`https://us-central1-reviewtext-ad5c6.cloudfunctions.net/function-11?limit=${limit}`)
-    .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        try {
-       //   const parsedData = JSON.parse(data);
-          setData(data);
-        } catch (error) {
-          console.log("Invalid JSON format:", error);
-        }
-      })
-      .catch((err) => console.log(err));
-  }, []);
+  const [inputText, setInputText] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
 
   return (
-    <div>
-      {data.map((item) => (
-        <div key={item.createdDateTime}>
-          <h3 style={{ color: "brown" }}> {item.createdDateTime} ---- model: <span style={{ color: "blue", fontSize: "24px" }}>{item.model}</span></h3>
-          <h4 style={{ color: "green" }}>{item.question}</h4>
-          <ReactMarkdown>{item.answer}</ReactMarkdown>
-        </div>
-      ))}
+    <div style={{ display: "flex", height: "100vh" }}>
+      <div style={{ width: "45%", height: "98%", padding: "10px" }}>
+        <textarea value={inputText} onChange={handleInputChange} style={{ width: "100%", height: "100%" }} />
+      </div>
+      <div style={{ width: "45%", height: "98%", padding: "10px" }}>
+        <ReactMarkdown>{inputText}</ReactMarkdown>
+      </div>
     </div>
   );
 };
